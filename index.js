64c3f0cf256feb44da2452e9270f0ecf61832277
belaeat <omyhossain@gmail.com>
2023-06-12 16:29:48 +0600

    cart collection working properly

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const cartCollection = client.db("saucyDb").collection("cart")
+
+        // inserting document to the cartCollection
+        app.post('/carts', async (req, res) => {
+            const item = req.body
+            console.log(item)
+            const result = await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
