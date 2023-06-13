e1a655413d5dda41484dd14f5fe32430599a9462
belaeat <omyhossain@gmail.com>
2023-06-13 20:02:15 +0600

    popular instructor data sorted and limited from server

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ async function run() {
             res.send(result)
         })
 
+        // popular classes API
+        app.get('/popularInstructors', async (req, res) => {
+            const query = {};
+            const options = {
+                sort: { students: -1 },
+            };
+
+            const limit = parseInt(req.query.limit) || 6;  
+            const skip = parseInt(req.query.skip) || 0;
+            const result = await instructorsCollection.find(query, options).limit(limit).skip(skip).toArray()
+            res.send(result)
+        })
+
         // classes API
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray()
