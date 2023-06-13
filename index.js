57931d3945684c3d6107d590a646fc64f4f6be04
belaeat <omyhossain@gmail.com>
2023-06-13 22:07:19 +0600

    delete api working properly

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 // mongodb connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrgajyt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -82,6 +82,13 @@ async function run() {
             res.send(result)
         })
 
+        // cart item delete API
+        app.delete('/carts/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
 
         app.post('/carts', async (req, res) => {
             const item = req.body
