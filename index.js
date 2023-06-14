53418ddad54757175b4a9d302efd82cda14437d0
belaeat <omyhossain@gmail.com>
2023-06-14 16:59:16 +0600

    admin route secured

--- index.js
+++ index.js
@@ -41,6 +41,17 @@ async function run() {
             res.send({ token })
         })
 
+        // verify admin middleware
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+            next();
+        }
+
         // verify JWT
         const verifyJWT = (req, res, next) => {
             const authorization = req.headers.authorization;
@@ -60,7 +71,7 @@ async function run() {
         }
 
         // users API's
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
@@ -79,6 +90,20 @@ async function run() {
                 res.send(result);
             }
 
+        })
+
+        // verify admin 
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false });
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
+            res.send(result)
         })
 
         // make admin API
