b819d6672241f9560178961b83b9f482d311ab26
belaeat <omyhossain@gmail.com>
2025-03-01 02:26:14 +0200

    update: index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors({
-  origin: "http://localhost:5173/",
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // mongodb connection
